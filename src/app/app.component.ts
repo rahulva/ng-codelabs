1e7b9cd95cd3bfbfd62365ebc0125854e7379a29
Rahul V R <rahulv.devlog>
2024-03-16 11:58:14 +0100

    Changes before upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  imports: [HomeComponent],
+  template: `
+
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" area-hidden="true" />
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
